8719c4df4eecde4d9fcc89b4de050faab37c8ef8
nicholasarinduque <89489431+nicholasarinduque@users.noreply.github.com>
2021-11-29 15:47:47 -0800

    2.6 done started 2.7

--- Assets/Scrpits/PlayerController.cs
+++ Assets/Scrpits/PlayerController.cs
@@ -5,10 +5,28 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody rb;
+    private float movmentX;
+    private float movmentY;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+    }
 
+    void OnMove(InputValue movementValue) 
+    {
+    Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movmentX = movementVector.x;
+        movmentY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movment = new Vector3(movmentX, 0.0f, movmentY);
+      
+        rb.AddForce(movementVector);
     }
 }
-
